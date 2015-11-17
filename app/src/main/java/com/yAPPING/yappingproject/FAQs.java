7425c8f4f307ad81dfbd4b674119dd41f901acb8
srikar0711 <srikar0711@gmail.com>
2015-11-17 17:17:01 +0530

    Version 1.4 playstore

--- app/src/main/java/com/yAPPING/yappingproject/FAQs.java
+++ app/src/main/java/com/yAPPING/yappingproject/FAQs.java
@@ -8,6 +8,15 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by srikar on 10/9/15.
@@ -16,35 +25,53 @@ public class FAQs extends Activity {
 
 
     ListView faqlv;
-    String[] stringquesFAQ;
-    String[] stringansFAQ;
+    ArrayList<String> stringquesFAQ;
+    ArrayList<String> stringansFAQ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.faq);
-        stringquesFAQ = getResources().getStringArray(R.array.FAQQues);
-        stringansFAQ = getResources().getStringArray(R.array.FAQAns);
         faqlv = (ListView) findViewById(R.id.faqlistView);
+        stringansFAQ = new ArrayList<String>();
+        stringquesFAQ = new ArrayList<String>();
+//        stringquesFAQ = getResources().getStringArray(R.array.FAQQues);
+//        stringansFAQ = getResources().getStringArray(R.array.FAQAns);
+
+        ParseQuery<ParseObject> qans = new ParseQuery<ParseObject>("FAQData");
+        qans.orderByAscending("createdAt");
+        qans.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
+           for(int i=0;i<list.size();i++) {
+//               Toast.makeText(getApplicationContext(),list.size()+"",Toast.LENGTH_SHORT).show();
+               ParseObject ob = list.get(i);
+               stringansFAQ.add(ob.getString("Answers").toString());
+               stringquesFAQ.add(ob.getString("Questions").toString());
+           }
+
         faqlv.setAdapter(new FAQAdapter(stringquesFAQ,stringansFAQ));
+            }
+        });
+
 
 
     }
 }
 
 class FAQAdapter extends BaseAdapter{
-String[] FAQQues,FAQAns;
-    FAQAdapter(String[] listQ,String[] listA){
+    ArrayList<String> FAQQues,FAQAns;
+    FAQAdapter(ArrayList<String> listQ,ArrayList<String> listA){
         this.FAQQues=listQ;
         this.FAQAns=listA;
     }
     @Override
     public int getCount() {
-        return FAQQues.length;
+        return FAQQues.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return FAQQues[position];
+        return FAQQues.get(position);
     }
 
     @Override
@@ -57,9 +84,9 @@ String[] FAQQues,FAQAns;
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         convertView = inflater.inflate(R.layout.faq_row, null);
         TextView textViewQ = (TextView) convertView.findViewById(R.id.faq_row_ques_textview);
-        textViewQ.setText(FAQQues[position]);
+        textViewQ.setText(FAQQues.get(position));
         TextView textViewA = (TextView) convertView.findViewById(R.id.faq_row_ans_textview);
-        textViewA.setText(FAQAns[position]);
+        textViewA.setText(FAQAns.get(position));
         return convertView;
     }
 }
